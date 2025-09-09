521003632bb7a0ce585b5051e97d6be6026ab01d
Dron <dron@Drons-iMac.local>
2025-09-09 16:18:01 -0500

    actualizando el authService

--- Autenticacion-Frontend/security-front/src/app/app.config.ts
+++ Autenticacion-Frontend/security-front/src/app/app.config.ts
@@ -1,14 +1,41 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { APP_INITIALIZER, ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
+import { AuthService } from './core/services/auth.service';
+import { last, lastValueFrom } from 'rxjs';
+
+
+
+/**
+ * Factor a de inicializaci n de la aplicaci n que intenta refrescar la sesi n actual en segundo plano.
+ *
+ * Devuelve una funci n que se encarga de llamar al m todo {@link AuthService#trySilentRefresh} y devuelve su observable.
+ *
+ * Se utiliza en el APP_INITIALIZER para intentar refrescar la sesi n actual antes de que se inicie la aplicaci n.
+ *
+ * @param authService El servicio de autenticaci n que se utilizar  para intentar refrescar la sesi n actual.
+ * @returns una funci n que devuelve un observable que se completa cuando se ha ejecutado el intento de refresco de la sesi n.
+ */
+export function initializeAppFactory(authService: AuthService): () => Promise<any> {
+  return () => lastValueFrom(authService.trySilentRefresh());
+}
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(),
-    provideClientHydration()]
+    provideClientHydration(),
+
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeAppFactory,
+      deps: [AuthService],
+      multi: true
+    }
+  ]
 };
--- Autenticacion-Frontend/security-front/src/app/core/services/auth.service.ts
+++ Autenticacion-Frontend/security-front/src/app/core/services/auth.service.ts
@@ -1,10 +1,10 @@
-import { computed, inject, Injectable, signal } from '@angular/core';
-import axios from 'axios';
-import { UserDto } from '../models/user.model';
+import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
 import { AuthResponse, LoginRequest } from '../models/auth.model';
-import { Observable, tap } from 'rxjs';
+import { BehaviorSubject, catchError, finalize, map, Observable, of, tap } from 'rxjs';
+import { Router } from '@angular/router';
+;
 
 
 
@@ -26,36 +26,125 @@ export class AuthService {
   // el readonly es para indicar que la variable es de solo lectura
   private readonly apiUrl = environment.apiUrl;
 
-  // el inject  es un decorador que nos permite inyectar el servicio en cualquier lugar de la aplicacion
-  private htt = inject(HttpClient);
 
-  constructor() { }
+  // el inject  es un decorador que nos permite inyectar el servicio en cualquier lugar de la aplicacion
+  private http = inject(HttpClient);
+  private router = inject(Router);
+
+  //el BehaviorSubject es un observable que nos permite manejar el estado del usuario en tiempo real
+  private currentUserSubject = new BehaviorSubject<AuthResponse | null>(null);
+
+  // exponemos el estado como un observable publico de solo lectura
+  // los componentes se subcribiran a este observable para obtener el estado del usuario
+  public currentUser$ = this.currentUserSubject.asObservable();
+
+  // Guardamos el AccessToken en memoria. nunca en el localStorage
+  private accessToken: string | null = null;
+
+  // Flag para evitar múltiples llamadas de refresh al mismo tiempo
+  private isRefreshing = false;
+
+
+
 
   /**
-   * Inicia sesion con el servidor, guardando el token en el localStorage
-   * @param credential Credenciales de inicio de sesion
-   * @returns Un observable que emite la respuesta del servidor
-   *          con la informacion de la sesion iniciada y el token de acceso ejemplo: { accessToken: 'token' }
+   * Obtiene el valor actual del usuario autenticado.
+   *
+   * Devuelve el valor actual del observable currentUser$.
+   * Si el usuario no est  autenticado, devuelve null.
+   *
+   * @returns el valor actual del usuario autenticado o null si no est  autenticado.
    */
-  login(credential: LoginRequest): Observable<AuthResponse> {
+  public get currentUserValue(): AuthResponse | null {
+    return this.currentUserSubject.getValue();
+  }
 
-    return this.htt.post<AuthResponse>(`${this.apiUrl}/auth/login`, credential).pipe(
+  public get getAccessToken(): string | null {
+    return this.accessToken;
 
-      // el tap es un operador que nos permite manipular la respuesta del servidor
-      tap(respose => {
-        localStorage.setItem('accessToken', respose.accessToken)
+  }
+
+  login(LoginRequest: LoginRequest): Observable<AuthResponse> {
+
+    return this.http.post<AuthResponse>(`${this.apiUrl}/auth/login`, LoginRequest).pipe(
+
+      tap(response => {
+
+        this.accessToken = response.accessToken;
+        this.currentUserSubject.next(response); // actualizamos el observable con el nuevo valor
+        console.log('Login exitoso!', response);
+        this.router.navigate(['/products']);
+
       })
     );
   }
 
-  logout(): void {
-    localStorage.removeItem('accessToken');
+
+
+  /**
+   * Cierra la sesi n actual del usuario.
+   *
+   * Hace una petici n HTTP POST al endpoint /logout para que el backend invalide la cookie HttpOnly.
+   * Luego, pase lo que pase ( xito o error), elimina el accessToken y el usuario actual, y redirige al usuario a la p gina de login.
+   *
+   * @returns un observable que se completa cuando se ha ejecutado el logout.
+   */
+  logout() {
+    // Llamamos al endpoint de logout del backend para que invalide la cookie HttpOnly
+    this.http.post(`${this.apiUrl}/auth/logout`, {}).pipe(
+      finalize(() => {
+        // Esto se ejecuta SIEMPRE (éxito o error)
+        this.accessToken = null;
+        this.currentUserSubject.next(null);
+        this.router.navigate(['/login']);
+      })
+    ).subscribe({
+      next: () => console.log('Logout exitoso en el backend'),
+      error: (err) => console.error('Error en el logout', err),
+      complete: () => console.log('Logout completado')
+    });
+
   }
 
-  isAuthenticated(): boolean {
-    return !!localStorage.getItem('accessToken');
-  }
 
+  /**
+   * Intenta refrescar la sesi n actual en segundo plano.
+   *
+   * Si la sesi n actual est  vencida, intenta refrescarla llamando al endpoint /refresh.
+   * Si el refresco tiene   xito, actualiza el accessToken y el usuario actual, y devuelve un observable que emite true.
+   * Si el refresco falla (porque el usuario no est  autenticado), elimina el accessToken y el usuario actual, y devuelve un observable que emite false.
+   *
+   * Se utiliza en el APP_INITIALIZER para intentar refrescar la sesi n actual antes de que se inicie la aplicaci n.
+   *
+   * @returns un observable que emite true si el refresco es exitoso, o false si falla.
+   */
+  trySilentRefresh(): Observable<boolean> {
+
+    if (this.isRefreshing) {
+      return of(false); // Evita llamadas concurrentes
+    }
+    this.isRefreshing = true;
+
+    return this.http.post<AuthResponse>(`${this.apiUrl}/auth/refresh`, {}).pipe(
+      tap({
+        next: response => {
+          this.accessToken = response.accessToken;
+          this.currentUserSubject.next(response);
+          this.isRefreshing = false;
+          console.log('Refresh silencioso exitoso.');
+        },
+        error: () => {
+          this.accessToken = null;
+          this.currentUserSubject.next(null);
+          this.isRefreshing = false;
+          console.log('No hay sesión activa para refrescar.');
+        }
+      }),
+      // Convertimos la respuesta a un booleano para el APP_INITIALIZER
+      map(() => true),
+      catchError(() => of(false))
+    );
+  }
 
 
 
